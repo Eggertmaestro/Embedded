3b688da222040a2da1e2f80a628a5f03f763d5c4
Eggertmaestro <54946803+Eggertmaestro@users.noreply.github.com>
2024-09-05 19:35:36 +0000

    Update encoder.cpp
    correcting error

--- Project 1 - Rotary encoder driver/Part 3/src/encoder.cpp
+++ Project 1 - Rotary encoder driver/Part 3/src/encoder.cpp
@@ -2,7 +2,7 @@
 #include <avr/io.h>
 
 Encoder::Encoder(int pin1_num, int pin2_num) : pin1(pin1_num, 'B'), pin2(pin2_num, 'D'){
-         EICRA |= (1 << ISC01); //Sense interrupt on pin0 change rising edge
+         EICRA |= (1 << ISC01); //Sense interrupt on pin0 change falling edge
          EIMSK |= (1 << INT0);  //Enable interrupt on INT0
         
         sei(); // enable interrupts
